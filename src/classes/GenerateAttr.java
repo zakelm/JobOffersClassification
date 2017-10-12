865eaee7d8e9e3f41966a6fe96abeb612d7eaf24
zakelm <zakelm@users.noreply.github.com>
2017-10-12 15:54:44 +0200

    Update GenerateAttr.java

--- src/classes/GenerateAttr.java
+++ src/classes/GenerateAttr.java
@@ -6,52 +6,45 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import org.apache.commons.io.FileUtils;
 
-/**
- * Class to generate attributes arrf files
- *
- */
 public class GenerateAttr {
-
 	public PrintWriter writer;
 	
 	public void writeAttr( String[] entires ) {
-		for( int i = 0; i < entires.length; i++ ) {
-			writer.println( entires[ i ] );
-			writer.flush();
-		}
+	    for( int i = 0; i < entires.length; i++ ) {
+		writer.println( entires[ i ] );
+		writer.flush();
+	    }
 	}
 
 	public void arffHeader( String file, String classes, String classeType ) throws FileNotFoundException {
-			writer = new PrintWriter(file);
-			
-			writer.println( "@relation textClassifier" );
-			writer.print( "\n" );
-			writer.println( "@attribute textClassifier {" + classes.replace( "[", "" ).replace( "]", "" ).trim() + "}" );
-			writer.println( "@attribute " + classeType.replace( "[", "" ).replace( "]", "" ).replace( ",", "" ) );//this is just the easy way you may need to replace those to text String
-			writer.print( "\n" );
-			writer.println( "@data" );
-			writer.flush();
+	    writer = new PrintWriter(file);
+	    writer.println( "@relation textClassifier" );
+	    writer.print( "\n" );
+	    writer.println( "@attribute textClassifier {" + classes.replace( "[", "" ).replace( "]", "" ).trim() + "}" );
+	    writer.println( "@attribute " + classeType.replace( "[", "" ).replace( "]", "" ).replace( ",", "" ) );
+	    writer.print( "\n" );
+	    writer.println( "@data" );
+	    writer.flush();
 	}
 	
 	public String[] getClassNames( String dataPath ) {
-		String[] subdirectories = new File( dataPath ).list();
-		return subdirectories;
+	    String[] subdirectories = new File( dataPath ).list();
+	    return subdirectories;
 	}
 		
 	public String[] readFiles( String labelPath, String Label ) throws IOException {
+	    File folder        = new File( labelPath + File.separator + Label );
+	    File[] filesList   = folder.listFiles();
+	    String[] lines     = new String[ filesList.length ];
 
-		File folder        = new File( labelPath + File.separator + Label );
-		File[] filesList   = folder.listFiles();
-		String[] lines     = new String[ filesList.length ];
+	    for ( int i = 0; i < filesList.length; i++ ) {
+		File file = filesList[ i ];
 
-		for ( int i = 0; i < filesList.length; i++ ) {
-			File file = filesList[ i ];
-
-			if ( file.isFile() && file.getName().endsWith( ".txt" ) ) {
-				String content = FileUtils.readFileToString(file);
-				lines[i] = Label + "," + "'" + content.replaceAll( "\n", "" ).replaceAll( "\r", "" ).replaceAll( ",", " " ).replaceAll( "'", " " ).replace( "_", " " ).replace( "-", " " ).replace( "&", " " ).replace( "%", " " ).replaceAll( " +", " " ).trim() + "'";
-			}
+		if ( file.isFile() && file.getName().endsWith( ".txt" ) ) {
+		    String content = FileUtils.readFileToString(file);
+		    lines[i] = Label + "," + "'" + content.replaceAll( "\n", "" ).replaceAll( "\r", "" ).replaceAll( ",", " " ).replaceAll( "'", " " ).replace( "_", " " ).replace( "-", " " ).replace( "&", " " ).replace( "%", " " ).replaceAll( " +", " " ).trim() + "'";
 		}
-		return lines;
+	    }
+	    return lines;
 	}
 }
