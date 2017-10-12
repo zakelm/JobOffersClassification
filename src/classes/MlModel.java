c3b3f4ca1d7fad0b7c05c662a528d562496a24a4
zakelm <zakelm@users.noreply.github.com>
2017-10-12 15:52:12 +0200

    Update MlModel.java

--- src/classes/MlModel.java
+++ src/classes/MlModel.java
@@ -22,98 +22,87 @@ public class MlModel {
 	StringToWordVector filter;
 	FilteredClassifier classifier;
 		
-	public void loadArff(String fileName) {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			ArffReader arff = new ArffReader(br);
-			trainData = arff.getData();
-			System.out.println("Loaded dataset  " + fileName);
-			br.close();
-		}
-		catch (IOException e) {
-			System.out.println(fileName);
-			
-		}
+	public void loadArff( String fileName ) {
+	    try {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		ArffReader arff = new ArffReader(br);
+		trainData = arff.getData();
+		System.out.println("Data chargés  " + fileName);
+		br.close();
+	    }
+	    catch (IOException e) {
+		System.out.println( "Problème trouvé à " + fileName );			
+	    }
 	}
 	
 	public void evaluate() {
-		try {
-			trainData.setClassIndex(0);			
-			filter = new StringToWordVector();			
-			filter.setAttributeIndices("last");
-			
-			NGramTokenizer tokenizer=new NGramTokenizer();
-			tokenizer.setNGramMinSize(1);
-			tokenizer.setNGramMaxSize(3);
-			tokenizer.setDelimiters(" \r\n\t.,;:'\"()?!'");
-			
-			filter.setTokenizer(tokenizer);
-			filter.setUseStoplist(false);
-			filter.setLowerCaseTokens(true);
-			filter.setOutputWordCounts(true);
-			filter.setWordsToKeep(1000);
-			
-			classifier = new FilteredClassifier();
-			classifier.setFilter(filter);
-			classifier.setClassifier(new SMO());
-			
-			Evaluation eval = new Evaluation(trainData);
-			eval.crossValidateModel(classifier, trainData, 4, new Random(1));
-			
-			System.out.println(eval.toSummaryString());
-			System.out.println(eval.toClassDetailsString());
-			System.out.println("Dataset evaluated");
+	    try {
+		trainData.setClassIndex( 0 );			
+		filter = new StringToWordVector();			
+		filter.setAttributeIndices( "last" );
+		NGramTokenizer tokenizer=new NGramTokenizer();
+		tokenizer.setNGramMinSize( 1 );
+		tokenizer.setNGramMaxSize( 3 );
+		tokenizer.setDelimiters( " \r\n\t.,;:'\"()?!'" );
+		filter.setTokenizer( tokenizer );
+		filter.setUseStoplist( false );
+		filter.setLowerCaseTokens( true );
+		filter.setOutputWordCounts( true );
+		filter.setWordsToKeep( 1000 );
+		classifier = new FilteredClassifier();
+		classifier.setFilter( filter );
+		classifier.setClassifier( new SMO() );
+		Evaluation eval = new Evaluation( trainData );
+		eval.crossValidateModel( classifier, trainData, 4, new Random( 1 ) );
+		System.out.println( eval.toSummaryString() );
+		System.out.println( eval.toClassDetailsString() );
+		System.out.println( "Evaluation du modèle" );
 						
-		}
-		catch (Exception e) {
-			System.err.println(e.getStackTrace());
-		}
+	    }
+	    catch (Exception e) {
+		System.err.println( "Problème trouvé " + e.getStackTrace() );
+	    }
 	}	
 	
 	public void train() {
-		try {
-			trainData.setClassIndex(0);
-			filter = new StringToWordVector();
-			filter.setAttributeIndices("last");
-			
-			NGramTokenizer tokenizer=new NGramTokenizer();
-			tokenizer.setNGramMinSize(1);
-			tokenizer.setNGramMaxSize(3);
-			tokenizer.setDelimiters(" \r\n\t.,;:'\"()?!'");
-			
-			filter.setTokenizer(tokenizer);
-			filter.setUseStoplist(false);
-			filter.setLowerCaseTokens(true);
-			filter.setOutputWordCounts(true);
-			filter.setWordsToKeep(1000);		
-			
-			classifier = new FilteredClassifier();
-			classifier.setFilter(filter);
-			classifier.setClassifier(new SMO());
-			classifier.buildClassifier(trainData);
-			
-			Instances trainingDataFiltered = weka.filters.Filter.useFilter(trainData, filter); // filter training data
-			
-			ArffSaver saver = new ArffSaver();
-			saver.setInstances(trainingDataFiltered);
-			saver.setFile(new File("attributes/MyFilteredLearner.arff"));
-			saver.writeBatch();
-			
-			System.out.println("Training dataset");
-		}
-		catch (Exception e) {
-			System.err.println(e.getStackTrace());
-		}
+	    try {
+		trainData.setClassIndex( 0 );
+		filter = new StringToWordVector();
+		filter.setAttributeIndices( "last" );
+		NGramTokenizer tokenizer=new NGramTokenizer();
+		tokenizer.setNGramMinSize( 1 );
+		tokenizer.setNGramMaxSize( 3 );
+		tokenizer.setDelimiters( " \r\n\t.,;:'\"()?!'" );
+		filter.setTokenizer( tokenizer );
+		filter.setUseStoplist( false );
+		filter.setLowerCaseTokens( true );
+		filter.setOutputWordCounts( true );
+		filter.setWordsToKeep( 1000 );		
+		classifier = new FilteredClassifier();
+		classifier.setFilter( filter );
+		classifier.setClassifier( new SMO() );
+		classifier.buildClassifier( trainData );
+		// filter training data
+		Instances trainingDataFiltered = weka.filters.Filter.useFilter( trainData, filter ); 
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances( trainingDataFiltered );
+		saver.setFile( new File( "attributes/MyFilteredLearner.arff" ) );
+		saver.writeBatch();
+		System.out.println( "Apprentissage" );
+	    }
+	    catch (Exception e) {
+		System.err.println( "Problème trouvé " + e.getStackTrace() );
+	    }
 	}	
 	
-	public void saveModel(String fileName) {
-		try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(fileName));
-            out.writeObject(classifier);
-            out.close();
-        } 
-		catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
+	public void saveModel( String fileName ) {
+	    try {
+                ObjectOutputStream out = new ObjectOutputStream( new FileOutputStream( fileName ) );
+                out.writeObject( classifier );
+                out.close();
+            } 
+	    catch (IOException e) {
+		System.out.println( "Problème trouvé " + e.getMessage() );
+	    }
 	}
 }
